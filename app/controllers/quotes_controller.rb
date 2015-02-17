d48ef353a6961ad33d266fc1e5511b5c6716fe36
Stefania Ferullo Cardenas <stefania.cardenas@bcsg.com>
2015-02-17 18:08:32 +0000

    you can create new instances of the model

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,13 +5,23 @@ class QuotesController < Runways::Controller
     render :index, :quotes => quotes
   end
 
-  # def a_quote
-  #   render :a_quote, :noun => :winking
-  # end
-  #
-  # def quote_1
-  #   quote_1 = Runways::Model::FileModel.find(1)
-  #   render :quote, :obj => quote_1
-  # end
+  def a_quote
+    render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = Runways::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def new_quote
+    attrs = {
+        "submitter" => "web user",
+        "quote" => "A picture is worth a thousand pixels",
+        "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
 	
 end
