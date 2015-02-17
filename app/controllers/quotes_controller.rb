e219f025563dd27d88a4731327145620de8ee853
Stefania Ferullo Cardenas <stefania.cardenas@bcsg.com>
2015-02-17 15:20:51 +0000

    you can see and index of quotes and a view with quotes as json

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,17 @@
 class QuotesController < Runways::Controller
 
-	def a_quote
-    render :a_quote, :noun => :winking
-	end
+	def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  # def a_quote
+  #   render :a_quote, :noun => :winking
+  # end
+  #
+  # def quote_1
+  #   quote_1 = Runways::Model::FileModel.find(1)
+  #   render :quote, :obj => quote_1
+  # end
 	
 end
